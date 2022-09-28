9388a70b8eb6a2434d1fbed1ee3db4ecd60a4e6b
Tanzeem <95355941+TMaliat@users.noreply.github.com>
2022-09-28 22:36:13 +0600

    Update fib.cpp

--- dp/fib.cpp
+++ dp/fib.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 const int N=1e5+10;
 
+int fib_tab(int n){
+    vector <long long> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        for (long long i=2; i<=n; i++)
+        {
+            dp[i] = (dp[i-1] + dp[i-2])%1000000007;
+        }return dp[n];
+}
+
 int dp[N];
 
 int fib(int n){
@@ -18,4 +28,6 @@ int main(){
     int n; cin >> n;
     for(int i=0;i<N;i++) dp[i]=-1;
     cout << fib(n) << endl;
+    cout<<"Using tabulation method" << endl;
+    cout <<fib_tab(n) << endl;
 }
