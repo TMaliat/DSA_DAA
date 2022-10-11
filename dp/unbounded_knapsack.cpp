646d353ae416d599995a63ec84fddf9c1acc3654
Tanzeem <95355941+TMaliat@users.noreply.github.com>
2022-10-11 14:13:06 +0600

    Create unbounded_knapsack.cpp

---
+++ dp/unbounded_knapsack.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int t[100][100];
+
+int knapsack_unbounded(int wt[],int val[],int w,int n){
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<w+1;j++){
+            t[i][j]=0;
+        }
+    }
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<w+1;j++){
+            if(wt[i-1]<=j) t[i][j]=max(val[i-1]+t[i][j-wt[i-1]],t[i-1][j]);
+            else t[i][j]=t[i-1][j];
+        }
+    }
+    return t[n][w];
+}
+
+int main(){
+    int wt[100],val[100],w,n; cin >> n >> w;
+    for(int i=0;i<n;i++){
+        cin >> wt[i] ;
+    }
+    for(int i=0;i<n;i++){
+        cin >> val[i];
+    }
+    int profit=knapsack_unbounded(wt,val,w,n);
+    cout << profit << endl;
+}
+
+//answer was supposed to be 300 but 500 is the output
